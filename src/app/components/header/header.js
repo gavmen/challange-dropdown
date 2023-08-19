e3e4cc23b0a02504e2311e3310cf8a91686fd842
Gabriel Mendonca <gabriel.mendonca@okn.com.br>
2023-08-19 01:20:04 -0300

    some coisas

---
+++ src/app/components/header/header.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styles from '../../styles/header.module.scss';
+import Image from 'next/image';
+
+const Header = () => {
+  const renderSubMenu = (items) => (
+    <div className={styles.subMenu}>
+      {items.map((item, index) => (
+        <p key={index} className={styles.subLink}>
+          <Image src={`/images/icon-${item.icon}.svg`} alt={item.icon} width={24} height={24} /> {item.label}
+        </p>
+      ))}
+    </div>
+  );
+
+  const navItems = [
+    {
+      label: 'Features',
+      icon: 'arrow-down',
+      subMenu: [
+        { label: 'Todo List', icon: 'todo' },
+        { label: 'Calendar', icon: 'calendar' },
+        { label: 'Reminders', icon: 'reminders' },
+        { label: 'Planning', icon: 'planning' },
+      ],
+    },
+    {
+      label: 'Company',
+      icon: 'arrow-down',
+      subMenu: [
+        { label: 'History' },
+        { label: 'Our Team' },
+        { label: 'Blog' },
+      ],
+    },
+    { label: 'Careers' },
+    { label: 'About' },
+  ];
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.navContainer}>
+        <div className={styles.navLeft}>
+          <div className={styles.navLogo}>
+            <Image src="/images/logo.svg" alt="logo" width={100} height={50} />
+          </div>
+          <ul className={styles.navLinks}>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                {item.label}
+                {item.icon && <Image src={`/images/icon-${item.icon}.svg`} className={styles.arrow} alt={item.icon} width={24} height={24} />}
+                {item.subMenu && renderSubMenu(item.subMenu)}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.mobileMenuBtn}>
+          <Image src="/images/icon-menu.svg" alt="menu" width={24} height={24} />
+        </div>
+        <div className={styles.mobileMenu}>
+          <ul className={styles.mobileLinks}>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <p>
+                  {item.label}
+                  {item.icon && <Image src={`/images/icon-${item.icon}.svg`} className={styles.arrow} alt={item.icon} width={24} height={24} />}
+                </p>
+                {item.subMenu && renderSubMenu(item.subMenu)}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.navRight}>
+          <button className={styles.transparentBtn}>Login</button>
+          <button className={styles.outlinedBtn}>Register</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/app/components/main/main.js
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import styles from '../../styles/mainSection.module.scss';
+
+const { 
+  mainContainer, 
+  mainLeft, 
+  mainHeading, 
+  mainContent, 
+  primaryBtn, 
+  logoes, 
+  mainRight 
+} = styles;
+
+const MainSection = () => {
+  return (
+    <main className={mainContainer}>
+      <section className={mainLeft}>
+        <h1 className={mainHeading}>
+          Make <br />
+          remote work
+        </h1>
+        <p className={mainContent}>
+          Get your team in sync, no matter your location. Streamline
+          processes, create team rituals, and watch productivity soar.
+        </p>
+        <button className={primaryBtn}>Learn more</button>
+        <div className={logoes}>
+          <Image src="/images/client-databiz.svg" alt="Databiz logo" width={100} height={100} />
+          <Image src="/images/client-audiophile.svg" alt="Audiophile logo" width={100} height={100} />
+          <Image src="/images/client-meet.svg" alt="Meet logo" width={100} height={100} />
+          <Image src="/images/client-maker.svg" alt="Maker logo" width={100} height={100} />
+        </div>
+      </section>
+      <aside className={mainRight}>
+        <picture>
+          <source media="(max-width:800px)" srcSet="/images/image-hero-mobile.png" />
+          <img src="/images/image-hero-desktop.png" alt="Hero illustration" />
+        </picture>
+      </aside>
+    </main>
+  );
+}
+
+export default MainSection;
